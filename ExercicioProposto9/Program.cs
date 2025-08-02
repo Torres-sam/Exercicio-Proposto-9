4fc298a641ab12e5d7a0862ae55e126225922b76
Samuel Torres <samuelvianatorres@gmail.com>
2025-08-01 21:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace MyApp
+{
+    internal class ExercicioProposto9
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite os Dois valores: ");
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);           
+            int b = int.Parse(valores[1]);
+
+
+            if (a % b == 0 || b % a == 0) {
+                Console.WriteLine("O primeiro número é múltiplo do segundo.");
+            } 
+            else {
+                Console.WriteLine("O primeiro número não é múltiplo do segundo.");
+
+            }
+    
+
+        }
+    }
+}
